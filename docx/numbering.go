6e1a733c782267e515f089006883801abbe21583
James Orson <jamesaorson@gmail.com>
2025-08-15 08:13:19 -0700

    autobutler: Fix name
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- docx/numbering.go
+++ docx/numbering.go
@@ -8,65 +8,65 @@ import (
 
 // This element specifies the contents of a main document part in a WordprocessingML document.
 type Numbering struct {
-	XMLName          xml.Name `xml:"w:numbering"`
-	Namespace_a      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:a,attr"`
-	Namespace_c      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:c,attr"`
-	Namespace_cr     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:cr,attr"`
-	Namespace_dgm    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:dgm,attr"`
-	Namespace_lc     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:lc,attr"`
-	Namespace_m      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:m,attr"`
-	Namespace_mc     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:mc,attr"`
-	Namespace_o      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:o,attr"`
-	Namespace_pic    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:pic,attr"`
-	Namespace_r      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:r,attr"`
-	Namespace_sl     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:sl,attr"`
-	Namespace_v      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:v,attr"`
-	Namespace_w      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w,attr"`
-	Namespace_w10    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w10,attr"`
-	Namespace_w14    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w14,attr"`
-	Namespace_w15    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w15,attr"`
-	Namespace_w16    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16,attr"`
-	Namespace_w16cex string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16cex,attr"`
-	Namespace_w16cid string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16cid,attr"`
-	Namespace_wne    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wne,attr"`
-	Namespace_wp     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wp,attr"`
-	Namespace_wpg    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wpg,attr"`
-	Namespace_wps    string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wps,attr"`
-	Namespace        string   `xml:"xmlns,attr"`
-	AbstractNum      *AbstractNum
-	Num              *Num
-	RelativePath     string `xml:"-"` // RelativePath is the path to the numbering file within the document package.
+	XMLName      xml.Name `xml:"w:numbering"`
+	XMLNSa       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:a,attr"`
+	XMLNSc       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:c,attr"`
+	XMLNScr      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:cr,attr"`
+	XMLNSdgm     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:dgm,attr"`
+	XMLNSlc      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:lc,attr"`
+	XMLNSm       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:m,attr"`
+	XMLNSmc      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:mc,attr"`
+	XMLNSo       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:o,attr"`
+	XMLNSpic     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:pic,attr"`
+	XMLNSr       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:r,attr"`
+	XMLNSsl      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:sl,attr"`
+	XMLNSv       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:v,attr"`
+	XMLNSw       string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w,attr"`
+	XMLNSw10     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w10,attr"`
+	XMLNSw14     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w14,attr"`
+	XMLNSw15     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w15,attr"`
+	XMLNSw16     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16,attr"`
+	XMLNSw16cex  string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16cex,attr"`
+	XMLNSw16cid  string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:w16cid,attr"`
+	XMLNSwne     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wne,attr"`
+	XMLNSwp      string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wp,attr"`
+	XMLNSwpg     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wpg,attr"`
+	XMLNSwps     string   `xml:"http://www.w3.org/2000/xmlns/ xmlns:wps,attr"`
+	Namespace    string   `xml:"xmlns,attr"`
+	AbstractNum  *AbstractNum
+	Num          *Num
+	RelativePath string `xml:"-"` // RelativePath is the path to the numbering file within the document package.
 }
 
 func NewNumbering(abstractNumId int, isOrdered bool) *Numbering {
 	return &Numbering{
 		// XMLName:          xml.Name{Local: "numbering"},
-		AbstractNum:      NewAbstractNum(abstractNumId, isOrdered),
-		Num:              NewNum(1, 1),
-		Namespace_mc:     "http://schemas.openxmlformats.org/markup-compatibility/2006",
-		Namespace_o:      "urn:schemas-microsoft-com:office:office",
-		Namespace_r:      "http://schemas.openxmlformats.org/officeDocument/2006/relationships",
-		Namespace_m:      "http://schemas.openxmlformats.org/officeDocument/2006/math",
-		Namespace_v:      "urn:schemas-microsoft-com:vml",
-		Namespace_wp:     "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing",
-		Namespace_w10:    "urn:schemas-microsoft-com:office:word",
-		Namespace_w:      "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
-		Namespace_wne:    "http://schemas.microsoft.com/office/word/2006/wordml",
-		Namespace_sl:     "http://schemas.openxmlformats.org/schemaLibrary/2006/main",
-		Namespace_a:      "http://schemas.openxmlformats.org/drawingml/2006/main",
-		Namespace_pic:    "http://schemas.openxmlformats.org/drawingml/2006/picture",
-		Namespace_c:      "http://schemas.openxmlformats.org/drawingml/2006/chart",
-		Namespace_lc:     "http://schemas.openxmlformats.org/drawingml/2006/lockedCanvas",
-		Namespace_dgm:    "http://schemas.openxmlformats.org/drawingml/2006/diagram",
-		Namespace_wps:    "http://schemas.microsoft.com/office/word/2010/wordprocessingShape",
-		Namespace_wpg:    "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup",
-		Namespace_w14:    "http://schemas.microsoft.com/office/word/2010/wordml",
-		Namespace_w15:    "http://schemas.microsoft.com/office/word/2012/wordml",
-		Namespace_w16:    "http://schemas.microsoft.com/office/word/2018/wordml",
-		Namespace_w16cex: "http://schemas.microsoft.com/office/word/2018/wordml/cex",
-		Namespace_w16cid: "http://schemas.microsoft.com/office/word/2016/wordml/cid",
-		Namespace_cr:     "http://schemas.microsoft.com/office/comments/2020/reactions",
-		Namespace:        "http://schemas.microsoft.com/office/tasks/2019/documenttasks",
+		AbstractNum: NewAbstractNum(abstractNumId, isOrdered),
+		Num:         NewNum(1, 1),
+		XMLNSmc:     "http://schemas.openxmlformats.org/markup-compatibility/2006",
+		XMLNSo:      "urn:schemas-microsoft-com:office:office",
+		XMLNSr:      "http://schemas.openxmlformats.org/officeDocument/2006/relationships",
+		XMLNSm:      "http://schemas.openxmlformats.org/officeDocument/2006/math",
+		XMLNSv:      "urn:schemas-microsoft-com:vml",
+		XMLNSwp:     "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing",
+		XMLNSw10:    "urn:schemas-microsoft-com:office:word",
+		XMLNSw:      "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
+		XMLNSwne:    "http://schemas.microsoft.com/office/word/2006/wordml",
+		XMLNSsl:     "http://schemas.openxmlformats.org/schemaLibrary/2006/main",
+		XMLNSa:      "http://schemas.openxmlformats.org/drawingml/2006/main",
+		XMLNSpic:    "http://schemas.openxmlformats.org/drawingml/2006/picture",
+		XMLNSc:      "http://schemas.openxmlformats.org/drawingml/2006/chart",
+		XMLNSlc:     "http://schemas.openxmlformats.org/drawingml/2006/lockedCanvas",
+		XMLNSdgm:    "http://schemas.openxmlformats.org/drawingml/2006/diagram",
+		XMLNSwps:    "http://schemas.microsoft.com/office/word/2010/wordprocessingShape",
+		XMLNSwpg:    "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup",
+		XMLNSw14:    "http://schemas.microsoft.com/office/word/2010/wordml",
+		XMLNSw15:    "http://schemas.microsoft.com/office/word/2012/wordml",
+		XMLNSw16:    "http://schemas.microsoft.com/office/word/2018/wordml",
+		XMLNSw16cex: "http://schemas.microsoft.com/office/word/2018/wordml/cex",
+		XMLNSw16cid: "http://schemas.microsoft.com/office/word/2016/wordml/cid",
+		XMLNScr:     "http://schemas.microsoft.com/office/comments/2020/reactions",
+		Namespace:   "http://schemas.microsoft.com/office/tasks/2019/documenttasks",
 	}
 }
 
